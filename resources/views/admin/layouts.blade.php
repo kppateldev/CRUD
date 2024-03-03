2e7bd28340cf907b2df9c98e4dcea507f34696ac
kppateldev <86559573+kppateldev@users.noreply.github.com>
2024-03-03 13:53:16 +0530

    Update layouts.blade.php

--- resources/views/admin/layouts.blade.php
+++ resources/views/admin/layouts.blade.php
@@ -22,11 +22,7 @@
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        @if(isset($user->name))
-                            {{$user->name}}
-                        @else
-                        Admin
-                        @endif
+                        {{ Auth::user()->name }}
                         </a>
                         <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="{{ route('admin.logout') }}"
